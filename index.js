ed663d5a37ff74199f2b05a2bddd042eb0e4f4c6
chaim mallakh <haym2054@gmail.com>
2018-01-09 01:26:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function happyHolidaysTo(name){
   return "Happy holidays, ${name}!";
 }
 function happyHolidaysTo(holiday,name){
-  return "(Happy ${holidays}, ${name}!);
+  return "(Happy ${holidays}, ${name}!)
 }
 
